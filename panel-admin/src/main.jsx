461013f2cc659436745ad9fddeb318dc1a9b5592
Yeiner Smith <yeinersmithquinterogarcia2007@gmail.com>
2026-02-24 21:44:13 -0500

    Se agrego el componente AdminPanel al src dentro de la carpeta components, ademas ya se unio con projectApp y main

--- panel-admin/src/main.jsx
+++ panel-admin/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import { ProjectApp } from './ProjectApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+    <ProjectApp/>
   </StrictMode>,
 )
