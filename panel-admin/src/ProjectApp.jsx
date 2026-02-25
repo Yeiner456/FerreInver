c83b304f8b39974c6390c7f3bebd6d9e18837bcc
Yeiner Smith <yeinersmithquinterogarcia2007@gmail.com>
2026-02-24 21:44:13 -0500

    Se agrego el componente AdminPanel al src dentro de la carpeta components, ademas ya se unio con projectApp y main

---
+++ panel-admin/src/ProjectApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { AdminPanel } from './components/AdminPanel'
+export const ProjectApp = () => {
+    return (
+        <div>
+            <AdminPanel/>
+        </div>
+    )
+}
