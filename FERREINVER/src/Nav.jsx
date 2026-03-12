a3a5b01135f56044aad4d7f90e164977c86665d4
Andres Algarin <andresf.algarin@gmail.com>
2026-03-11 22:42:14 -0500

    Version mejorada del index con carrusel funcional y links funcionales

--- FERREINVER/src/Nav.jsx
+++ FERREINVER/src/Nav.jsx
@@ -1,26 +1,46 @@
 import React from 'react'
 import './Styles/Nav.css'
+import { Link } from 'react-router-dom'
+
+
+
 
 export const Nav = () => {
   return (
     <nav>
 
         <img className='perfil' src="./public/assets/perfil.webp" alt="" />
+
+        <img  className='logo' src="./public/assets/logo.webp" alt="" />
             
             <ul className='nav-links'>
 
-                <li>
-                    <a className='inicio' href="">Inicio</a>
+                <li className='links'>
+                    <Link className='inicio' to="/">Inicio</Link>
+
                 </li>
 
-                <li>    
+                <li className='links'> 
+                <a className='productos' href="">Productos</a>
+            </li>
+
+            <li className='links'>
+                <a  className='contactanos' href="">Contactanos</a>
+            </li>
+
+            <li className='links'>
+
+                <Link className='quienes-somos' to="/quienes-somos">¿Quienes somos?</Link>
+            </li >
+
+                <li className='links'>    
                     <a  className='productos' href="">Productos</a>
                 </li>
 
-                <li >
+                <li className='links'>
                     <a  className='registro' href="">Registro</a>
                 </li>
-                <li>
+                <li className='links'>
                     <a  className='iniciar-sesion' href="">Iniciar Sesión</a>
                 </li>
             </ul>
---
+++ FERREINVER/src/Producto.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Styles/Producto.css'
+
+export const Producto = () => {
+  return (
+    <section className='seccion-productos'>
+
+      <h2 className='producto-title'>Productos</h2>
+
+      <div className='tarjetas'>
+        <div className='tarjeta-producto'></div>
+        <div className='tarjeta-producto'></div>
+        <div className='tarjeta-producto'></div>
+        <div className='tarjeta-producto'></div>
+        <div className='tarjeta-producto'></div>
+        <div className='tarjeta-producto'></div>
+      </div>
+
+    </section>
+  )
+}
