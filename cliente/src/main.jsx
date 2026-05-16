7a89eb4cba9b7975e5442e334792fa89849df2bc
Yeiner Smith <yeinersmithquinterogarcia2007@gmail.com>
2026-05-16 17:51:37 -0500

    error del deploy por el main

--- cliente/src/main.jsx
+++ cliente/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles/main.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ProjectApp } from './ProjectApp'
-import './Styles/main.css'
+import './styles/main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
